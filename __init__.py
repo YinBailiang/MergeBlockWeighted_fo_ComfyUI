da9a173b441914c7792e9a9fb6ed7a09975be16f
aurel-g <github@dev.bzh>
2024-12-15 23:19:29 +0100

    Allow using a different directory name

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-import custom_nodes.MergeBlockWeighted_fo_ComfyUI.script.MergeBlockWeightedNodes as MBWNodes
+from .script import MergeBlockWeightedNodes as MBWNodes
 
 NODE_CLASS_MAPPINGS = {
     "MergeBlockWeighted":MBWNodes.MergeBlockWeighted
